14b8590e29c3d131abc7624f38611e7badefc01c
xaviguasch <xaviguasch@gmail.com>
2021-02-22 15:50:55 +0100

    solved minor bugs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const closeButton = document.querySelector('.close-nav')
 const openButton = document.querySelector('.open-nav')
 const nav = document.querySelector('.header__nav')
+const link = document.querySelectorAll('.nav-link')
 
 closeButton.addEventListener('click', () => {
   nav.classList.remove('navigation-open')
@@ -8,4 +9,10 @@ closeButton.addEventListener('click', () => {
 
 openButton.addEventListener('click', () => {
   nav.classList.add('navigation-open')
+})
+
+link.forEach((event) => {
+  event.addEventListener('click', () => {
+    nav.classList.remove('navigation-open')
+  })
 })
